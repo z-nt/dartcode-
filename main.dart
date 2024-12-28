103b6e37b8fe01686f8408805ee60207c4191808
kian-nosratian <kiannosratian76@gmail.com>
2024-12-28 19:55:42 +0330

    practice dart language

---
+++ main.dart
@@ -0,0 +1,22 @@
+
+ List <Object> names =[];
+
+void person (String name , int age){
+//    var fullname = "$name \n $age";
+
+//      final List <Object> names= [];
+
+      var human = {
+         " firstname":name,
+          "age":age,
+      };
+
+        names.add(human);
+        print(names);
+}
+
+void main(){
+  person("kian", 27);
+}
+
+
